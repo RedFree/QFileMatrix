51ceb10f863adbe6a00efa70f2dc6a9a4192615f
wd19848 <wd19848@163.com>
2026-05-05 00:12:17 +0800

    ui: shrink camera view area slightly more
    - CameraViewWidget: reduce sizeHint 560x340 -> 520x320
    - CameraViewWidget: reduce minSize 420x260 -> 400x250

--- src/widgets/CameraViewWidget.cpp
+++ src/widgets/CameraViewWidget.cpp
@@ -11,7 +11,7 @@ CameraViewWidget::CameraViewWidget(QWidget *parent)
     : QWidget(parent)
     , m_refreshTimer(new QTimer(this))
 {
-    setMinimumSize(420, 260);
+    setMinimumSize(400, 250);
     setMouseTracking(true);
     connect(m_refreshTimer, &QTimer::timeout, this, qOverload<>(&CameraViewWidget::update));
     m_refreshTimer->setInterval(60);
@@ -208,7 +208,7 @@ void CameraViewWidget::mouseMoveEvent(QMouseEvent *event)
 
 QSize CameraViewWidget::sizeHint() const
 {
-    return QSize(560, 340);
+    return QSize(520, 320);
 }
 
 void CameraViewWidget::updateCrosshair(const QPoint &point)
